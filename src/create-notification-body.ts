fe7526bcefb8086b41c2bad668039d6fc66855b5
Augusto Monclaro <monclaro.a@gmail.com>
2022-12-12 23:06:49 -0300

    Initial commit

---
+++ src/create-notification-body.ts
@@ -0,0 +1,16 @@
+/* eslint-disable prettier/prettier */
+import { IsNotEmpty } from 'class-validator';
+import { IsUUID, Length } from 'class-validator/types/decorator/decorators';
+
+export class CreateNotificationBody {
+  @IsNotEmpty()
+  @IsUUID()
+  recipientId: string;
+
+  @IsNotEmpty()
+  @Length(5, 240)
+  content: string;
+
+  @IsNotEmpty()
+  category: string;
+}
